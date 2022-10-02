90b327c631666a053d5b5d439425ee6d70f6d03e
Aniebiet onoyom <aniebietonoyom>
2022-10-02 12:48:11 +0100

    same week report

--- sept.js
+++ sept.js
@@ -35,3 +35,24 @@ greet("Jude", morning);
 greet("Sam", morning);
 
 //Working on call back function
+
+
+
+//ARRAY ITERATIORS (for each, map, filter, reduce)
+
+/*FOR EACH
+For each does not return a new array
+*/
+
+const people = [
+    {name: 'bob', age: 30, position: 'developer'},
+    {name: 'Hanson', age: 100, position: 'designer'},
+    {name: 'Ime', age: 90, position: 'CEO'},
+]
+
+
+function showPerson(person){
+console.log(person);
+}
+
+people.forEach{showPerson}
