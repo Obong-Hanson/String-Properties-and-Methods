e9f516dbd461a2bc8098841f0d2d194c6100ec16
Aniebiet onoyom <aniebietonoyom>
2022-10-12 05:24:01 +0100

    started off with a new topic, DOM

---
+++ week2-oct.js
@@ -0,0 +1,11 @@
+//DOM is similar to CSS
+
+//getElementById
+//decide the efect you want to apply
+//make sure the IDs match. If you select the Id that does not exist, you get undefined.
+
+const h1 = document.getElementById('title');
+h1.style.color = `purple`
+
+document.getElementById('btn').style.backgroundColor = 'orange'
+document.getElementById('btn').style.color = 'green'
